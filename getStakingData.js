27eaa526250b43e8bdfe3c1e69f73b8596874e5e
Alberto Viera <albertoviera@gmail.com>
2021-09-06 18:02:28 +0200

    added staking file

---
+++ getStakingData.js
@@ -0,0 +1,35 @@
+/*
+  Simple code snippet to fetch parachain staking related data using 
+  Polkadot.js API - Not to be used in production
+  Use at your own discretion
+*/
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { typesBundle } from 'moonbeam-types-bundle';
+
+// Create Provider
+const wsProvider = new WsProvider('wss://wss.moonriver.moonbeam.network');
+
+const main = async () => {
+  // Wait for Provider
+  const api = await ApiPromise.create({
+    provider: wsProvider,
+    typesBundle: typesBundle,
+  });
+  await api.isReady;
+
+  // Get the list of active collators
+  const collatorList = (await api.query.parachainStaking.selectedCandidates()).toJSON();
+  console.log(collatorList);
+
+  // Get list of ALL collators
+  //const collatorList = (await api.query.parachainStaking.candidatePool()).toJSON();
+  //console.log(collatorList);
+
+  // Get details of each collator
+  for (let i = 0; i < collatorList.length - 1; i++) {
+    const collatorData = await api.query.parachainStaking.collatorState2(collatorList[i]);
+    console.log(collatorData.toJSON());
+  }
+};
+
+main();
