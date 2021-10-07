47f4d22ffa508b99a3887fc20ccfabb01e237587
albertov19 <64150856+albertov19@users.noreply.github.com>
2021-10-07 11:04:52 +0200

    Update verifyMessage.js

--- verifyMessage.js
+++ verifyMessage.js
@@ -6,12 +6,12 @@ const main = async () => {
   await cryptoWaitReady();
 
   const signedMessage =
-    '0x1689bf94696757552c0d934585f9f8a56e9341917ab14de5487aa607e1ccdc7a61ce27b29463f8873e0fe65545505967cea5643eb6f6fb9ece40beac73996f86';
+    'signed_bytes';
 
   //const message = stringToU8a('email');
-  const message = '0x4489c599cf046b2f0fcd32a89e96a4def2345d6f';
+  const message = 'account';
 
-  const address = 'Ei6hny8TPztPpF9ZBeKJWD24f4g2GTAVd2pLZwA3XPz7isT';
+  const address = 'KusamaAddress';
 
   // create the message, actual signature and verify
   const { isValid } = signatureVerify(message, signedMessage, address);
