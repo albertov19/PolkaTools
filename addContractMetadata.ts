938279d25cfcfef58ec733c3cd36e0d4c95e76e2
albertov19 <albertoviera@gmail.com>
2024-10-23 15:10:10 +0200

    force batch

--- addContractMetadata.ts
+++ addContractMetadata.ts
@@ -49,7 +49,7 @@ const main = async () => {
   }
 
   // Batch It
-  let batchTx = await api.tx.utility.batch(batchInner);
+  let batchTx = await api.tx.utility.forceBatch(batchInner);
 
   console.log(`Call add contract metadata`);
   console.log(batchTx.method.toHex());
